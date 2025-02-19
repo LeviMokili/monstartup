a698cd2c07185df897e24be1404e736243df4858
levimokili <levismp8d@gmail.com>
2025-02-19 23:45:38 +0300

    second push

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Plinedu',
+      title: 'Plinedu app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
